deed7031e144653631092b45c2f9ff1c11c0f6db
Anoxia <anoxia@engineer.com>
2022-09-27 17:10:27 +0800

    style: 清理代码

--- src/Http/Context.php
+++ src/Http/Context.php
@@ -39,11 +39,6 @@ class Context
      */
     public $contentType = 'application/json';
 
-    // /**
-    //  * @var Input
-    //  */
-    // public $input;
-
     /**
      * 上下文中间状态，用于在各个组件数据共享
      *
@@ -93,35 +88,4 @@ class Context
     {
         $this->contentType = $type;
     }
-
-    /**
-     * 为输入参数提供快捷访问实例，在业务处理时，
-     * 直接使用 $ctx->input 属性即可获取 queryString 参数与 form data 等参数
-     *
-     * 注：如果 query 参数与 form data 参数存在同名参数时
-     *     form data 参数将会覆盖 query 参数，若想获取正确 query 参数
-     *     请使用 $ctx->request->queryParams() 方法手动提取
-     */
-    // private function processInput(): Input
-    // {
-    //     $dataPayload = [];
-
-    //     $queryParams = $this->request->getQueryParams();
-    //     if ($queryParams) {
-    //         $dataPayload += $queryParams;
-    //     }
-
-    //     // 此处 parsedBody 可能是 array, object, null 中的一种
-    //     // 需要手动处理为 array 格式
-    //     $parsedBody = $this->request->getParsedBody();
-    //     if (is_object($parsedBody)) {
-    //         $parsedBody = objectToArray($parsedBody);
-    //     }
-
-    //     if ($parsedBody) {
-    //         $dataPayload += $parsedBody;
-    //     }
-
-    //     return new Input($dataPayload);
-    // }
 }
