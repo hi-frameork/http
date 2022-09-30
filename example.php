0ab9c84708b9cf10caf7b716e99aa63227502f7b
Anoxia <anoxia@engineer.com>
2022-09-30 16:37:32 +0800

    doc: 更新示例代码(example.php)

--- example.php
+++ example.php
@@ -1,12 +1,18 @@
 <?php
 
 use Hi\Http\Application;
+use Hi\Http\Context;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $app = new Application();
 
+// 注册中间件
+$app->use(function (Context $ctx, Closure $next) {
+    return $next($ctx);
+});
+
+// 路由定义
 $app->get('/', fn () => 'hi');
-$app->get('/time', fn () => time());
 
 $app->listen(4000);
