e97f59cbdada67ada625256d225eeebd47ebbc90
Anoxia <anoxia@engineer.com>
2022-09-30 16:39:09 +0800

    doc: 完善 example.php 文件代码注释

--- example.php
+++ example.php
@@ -5,6 +5,7 @@ use Hi\Http\Context;
 
 require __DIR__ . '/vendor/autoload.php';
 
+// 默认在 php-builtin 容器运行
 $app = new Application();
 
 // 注册中间件
@@ -15,4 +16,5 @@ $app->use(function (Context $ctx, Closure $next) {
 // 路由定义
 $app->get('/', fn () => 'hi');
 
+// 服务将会运行在 http://127.0.0.1:4000
 $app->listen(4000);
